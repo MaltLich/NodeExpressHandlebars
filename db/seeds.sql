9945d6322ede0b3eefd9004fcaa39ec39b8975d5
Andrew Novak <andrew.n.novak@gmail.com>
2021-02-12 19:08:07 -0500

    Update index.handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheesburger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Double Cheesburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado Cheesburger', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Meatless Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Cheesburger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', TRUE);
